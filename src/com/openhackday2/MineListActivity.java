8ba72516b21ca40cd15e2c8d11ca463bf9de2163
kingarthur <kingarthur@192.168.144.241>
2014-02-15 22:40:33 +0900

    add title and author

--- src/com/openhackday2/MineListActivity.java
+++ src/com/openhackday2/MineListActivity.java
@@ -1,12 +1,17 @@
 package com.openhackday2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import com.zxing.activity.CaptureActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -14,6 +19,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,7 +34,8 @@ import android.widget.TextView;
 
 public class MineListActivity extends Activity implements OnClickListener {
 	private ListView mListView;
-
+	private SharedPreferences prefs;
+	private Set<String> bookSet;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -52,6 +59,11 @@ public class MineListActivity extends Activity implements OnClickListener {
 			}
 		});
 
+		prefs = this.getSharedPreferences(
+				"com.openhackday2", Context.MODE_PRIVATE);
+		
+        bookSet = prefs.getStringSet("books", new HashSet<String>());
+        
 
 		// mViewMy.findViewById(R.id.list_tab_my_add).setOnClickListener(this);
 		findViewById(R.id.mine_list_add_button).setOnClickListener(this);
